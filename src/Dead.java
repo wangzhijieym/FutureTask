9d76edd273828352a41c9e598d32d9fec8641726
wangzhijieym <wzj510_wo@163.com>
2018-11-02 17:11:36 +0800

    test2

--- src/Dead.java
+++ src/Dead.java
@@ -8,6 +8,7 @@ public class Dead implements Runnable {
     public void run(){
         while (true){
             if(flag){
+                //aa
                 synchronized (Lock.loke1){
                     System.out.println("1 lock1");
                     synchronized (Lock.loke2){
